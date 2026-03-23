cb7f1c2a54496cddc8f81a81137414f3273f27f0
Pawan Raj <pawan3072003@gmail.com>
2026-03-23 18:16:23 +0530

    Time: 0 ms (100%), Space: 9.8 MB (61.29%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2 != str2+str1){
+            return "";
+        }
+        int n= str1.length();
+        int m= str2.length();
+        int l=__gcd(n,m);
+        return str1.substr(0,l);
+    } 
+};
