d7b304254191f939dbd5c2bf0486b46b123386e1
Pawan Raj <pawan3072003@gmail.com>
2026-07-14 03:18:05 +0530

    Time: 35 ms (18.7%), Space: 31.6 MB (14.89%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<vector<int>,vector<string>> mp;
+        for( string s : strs){
+            vector<int> freq(26,0);
+
+            for(char c: s){
+                freq[c-'a']++;
+            }
+            mp[freq].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for(auto &it : mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
